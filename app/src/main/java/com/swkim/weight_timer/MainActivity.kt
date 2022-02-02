84ca1536c0166577d887e538a15761fa70f56348
Seungwon <dnjs5397@naver.com>
2022-02-02 14:06:45 +0900

    Weight_Training_Timer

---
+++ app/src/main/java/com/swkim/weight_timer/MainActivity.kt
@@ -0,0 +1,202 @@
+package com.swkim.weight_timer
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.*
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.swkim.weight_timer.databinding.ActivityMainBinding
+import java.sql.Time
+import java.util.*
+import kotlin.concurrent.timer
+
+class MainActivity : AppCompatActivity() {
+    private var complete = false
+    private var time = 0
+    private var isRunning = false
+    private var timeTask: Timer? = null
+    private var timerTask : Timer? = null
+    private var timelap : Int = 1
+    private var isResting = false
+    var set : Int = 0
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val intent = intent
+        binding.goalSet.text = intent.getStringExtra("goal_set")
+        var restTime = intent.getStringExtra("rest_time")
+
+        start()
+        isRunning = !isRunning
+
+        binding.startButton.setOnClickListener {
+            if (!complete) {
+                isRunning = !isRunning
+                if (isRunning)
+                    start()
+                else
+                    pause()
+            }
+        }
+
+        // 홈 버튼 누르면 경고창 출력
+        binding.resetButton.setOnClickListener {
+            if (!complete) {
+                val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_design, null)
+                val builder = AlertDialog.Builder(this)
+                    .setView(dialogView)
+
+                val mAlertDialog = builder.show()
+
+                val okBtn = dialogView.findViewById<Button>(R.id.yes_btn)
+                okBtn.setOnClickListener {
+                    mAlertDialog.dismiss()
+                    finish()
+                }
+                val noBtn = dialogView.findViewById<Button>(R.id.no_btn)
+                noBtn.setOnClickListener {
+                    mAlertDialog.dismiss()
+                }
+            }
+            else {
+                finish()
+            }
+        }
+
+        binding.restButton.setOnClickListener {
+            if (!isResting && isRunning && set != Integer.parseInt(binding.goalSet.text.toString()) && !complete) {
+                isResting = !isResting
+                var goalset = Integer.parseInt(binding.goalSet.text.toString())
+                var restcount = Integer.parseInt(restTime)
+                set += 1
+                restcount += 1
+                if (set == goalset) {
+                    binding.leftRest.text = ""
+                    timeTask?.cancel()
+                    timerTask?.cancel()
+                    binding.weighInfo.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 22F)
+                    binding.goingSet.text = set.toString()
+                    binding.weighInfo.text = "수고하셨습니다!\n목표한 세트 수에 도달했습니다."
+                    isRunning = false
+                    isResting = true
+                    complete = true
+                }
+
+                else {
+                    binding.leftRest.text = "남은 휴식 시간"
+                    binding.weighInfo.text = restcount.toString()
+                    binding.goingSet.text = set.toString()
+                    timerTask = timer(period = 1000) {
+                        if (restcount == 0) {
+                            runOnUiThread {
+                                binding.leftRest.text = ""
+                                binding.weighInfo.text = "운동 중.."
+                                isResting = !isResting
+                            }
+                            this.cancel()
+                        }
+                        else {
+                            runOnUiThread {
+                                restcount -= 1
+                                binding.weighInfo.text = "$restcount"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    override fun onBackPressed() {
+        if (!complete) {
+            val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_design, null)
+            val builder = AlertDialog.Builder(this)
+                .setView(dialogView)
+
+            val mAlertDialog = builder.show()
+
+            val okBtn = dialogView.findViewById<Button>(R.id.yes_btn)
+            okBtn.setOnClickListener {
+                mAlertDialog.dismiss()
+                finish()
+            }
+            val noBtn = dialogView.findViewById<Button>(R.id.no_btn)
+            noBtn.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+        }
+        else {
+            finish()
+        }
+    }
+
+    private fun start() {
+        binding.startButton.setImageResource(R.drawable.ic_baseline_pause_24)
+        if (binding.weighInfo.text == "일시정지") {
+            binding.weighInfo.text = "운동 중.."
+        }
+        timeTask = timer(period = 10) {
+            time++
+            val min = (time / 6000) % 60
+            val sec = (time / 100) % 60
+            val ms = time % 100
+            runOnUiThread {
+                if (min<10) { // 분
+                    binding.minutes.text = "0$min"
+                }
+                else {
+                    binding.minutes.text = "$min"
+                }
+                if (sec < 10) { // 초
+                    binding.seconds.text = "0$sec"
+                } else {
+                    binding.seconds.text = "$sec"
+                }
+
+                if (ms < 10){ // m초
+                    binding.mSeconds.text = "0$ms"
+                }else {
+                    binding.mSeconds.text = "$ms"
+                }
+
+
+            }
+
+        }
+
+    }
+
+    private fun pause() {
+        if (isResting) {
+            binding.leftRest.text = ""
+            binding.startButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            timeTask?.cancel() // 실행중인 타이머가 있다면 취소
+            timerTask?.cancel()
+            isResting = false
+            binding.weighInfo.text = "일시정지"
+        }
+        else {
+            binding.startButton.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            timeTask?.cancel() // 실행중인 타이머가 있다면 취소
+            timerTask?.cancel()
+            isResting = false
+            binding.weighInfo.text = "일시정지"
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/swkim/weight_timer/Start_display.kt
@@ -0,0 +1,69 @@
+package com.swkim.weight_timer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.swkim.weight_timer.databinding.ActivityStartDisplayBinding
+
+class Start_display : AppCompatActivity() {
+    var set = 0
+    var rest = 0
+    var backPressedTime : Long = 0
+    private lateinit var binding : ActivityStartDisplayBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityStartDisplayBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val intent = Intent(this@Start_display, MainActivity::class.java)
+
+
+
+        binding.weightStart.setOnClickListener {
+            if (binding.setCount.text =="0" || binding.restCount.text == "0") {
+                Toast.makeText(this, "올바른 세트 수 또는 휴식 시간을 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                var goalSet: String = binding.setCount.text.toString()
+                var restSet: String = binding.restCount.text.toString()
+                intent.putExtra("goal_set", goalSet)
+                intent.putExtra("rest_time", restSet)
+                startActivity(intent)
+            }
+        }
+
+        binding.setUpButton.setOnClickListener{
+            set += 1
+            binding.setCount.text = set.toString()
+        }
+
+        binding.setDownButton.setOnClickListener{
+            if (set != 0) {
+                set -= 1
+                binding.setCount.text = set.toString()
+            }
+        }
+
+        binding.restUpButton.setOnClickListener{
+            rest += 10
+            binding.restCount.text = rest.toString()
+        }
+
+        binding.restDownButton.setOnClickListener{
+            if (rest != 0) {
+                rest -= 10
+                binding.restCount.text = rest.toString()
+            }
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() - backPressedTime < 2000) {
+            finish()
+            return
+        }
+        Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르시면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+        backPressedTime = System.currentTimeMillis()
+    }
+}
